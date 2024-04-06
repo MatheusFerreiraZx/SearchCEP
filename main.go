e50e89c42511ad7e943b340c782591007b7eecc5
Matheus Ferreira <Mathferreiranasc12@gmail.com>
2024-04-06 01:48:24 -0300

    feat: add cli interations

--- main.go
+++ main.go
@@ -2,67 +2,71 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
+	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
+	"os"
 )
 
 type CEP struct {
-	Cep         string `json:"cep"`
-	Logradouro  string `json:"logradouro"`
-	Complemento string `json:"complemento"`
-	Bairro      string `json:"bairro"`
-	Localidade  string `json:"localidade"`
-	Uf          string `json:"uf"`
-	Ibge        string `json:"ibge"`
-	Gia         string `json:"gia"`
-	Ddd         string `json:"ddd"`
-	Siafi       string `json:"siafi"`
+	Cep        string `json:"cep"`
+	Logradouro string `json:"logradouro"`
+	Bairro     string `json:"bairro"`
+	Localidade string `json:"localidade"`
+	Uf         string `json:"uf"`
+	Ibge       string `json:"ibge"`
+	Ddd        string `json:"ddd"`
+	Siafi      string `json:"siafi"`
 }
 
 func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", SearchHandlerCEP)
-	http.ListenAndServe(":8080", mux)
-}
+	cepFlag := flag.String("cep", "", "CEP para pesquisar")
+	flag.Parse()
 
-func SearchHandlerCEP(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		w.WriteHeader(http.StatusNotFound)
+	if *cepFlag == "" {
+		fmt.Println("Por favor, forneça um CEP usando -cep <cep>")
+		os.Exit(1)
 	}
 
-	cepParam := r.URL.Query().Get("cep")
-	if cepParam == "" {
-		w.WriteHeader(http.StatusBadRequest)
-		return
+	cep, err := SearchCEP(*cepFlag)
+	if err != nil {
+		fmt.Printf("Erro ao buscar CEP: %v\n", err)
+		os.Exit(1)
 	}
 
-	cep, error := SearchCEP(cepParam)
-	if error != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(cep)
+	fmt.Printf("Endereço correspondente ao CEP %s:\n", cep.Cep)
+	fmt.Printf("Logradouro: %s\n", cep.Logradouro)
+	fmt.Printf("Bairro: %s\n", cep.Bairro)
+	fmt.Printf("Localidade: %s\n", cep.Localidade)
+	fmt.Printf("UF: %s\n", cep.Uf)
+	fmt.Printf("IBGE: %s\n", cep.Ibge)
+	fmt.Printf("DDD: %s\n", cep.Ddd)
+	fmt.Printf("SIAFI: %s\n", cep.Siafi)
 }
 
 func SearchCEP(cep string) (*CEP, error) {
-	response, error := http.Get("https://viacep.com.br/ws/" + cep + "/json/")
-	if error != nil {
-		return nil, error
+	response, err := http.Get("https://viacep.com.br/ws/" + cep + "/json/")
+	if err != nil {
+		return nil, err
 	}
-	defer response.Body.Close()
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			fmt.Printf("Erro ao fechar o corpo da resposta: %v\n", err)
+		}
+	}(response.Body)
 
-	body, error := ioutil.ReadAll(response.Body)
-	if error != nil {
-		return nil, error
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return nil, err
 	}
 
 	var c CEP
-	error = json.Unmarshal(body, &c)
-	if error != nil {
-		return nil, error
+	err = json.Unmarshal(body, &c)
+	if err != nil {
+		return nil, err
 	}
 
 	return &c, nil
